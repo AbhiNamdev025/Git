5088519fcd1f66d4d9e943e14950003ae78d72a3
AbhiNamdev025 <namdevabhi025@gmail.com>
2025-08-11 16:22:19 +0530

    JS Started

---
+++ C-HUB/Aug/Aug-11/script.js
@@ -0,0 +1,33 @@
+// console.log("Table of Three");
+// for (let a = 3, i = 1; i <= 10; i++) {
+//   console.log(a * i);
+// }
+// console.log("Table of Four");
+// for (let a = 4, i = 1; i <= 10; i++) {
+//   console.log(a * i);
+// }
+// console.log("Table of Five");
+// for (let a = 5, i = 1; i <= 10; i++) {
+//   console.log(a * i);
+// }
+
+// Table Calulation
+
+function table(a) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(a * i);
+  }
+}
+
+for (let i = 2; i <= 5; i++) {
+  console.log("Table of :", i);
+  table(i);
+}
+
+// let a = prompt("Enter A Number :");
+// let i = 1;
+// console.log("Table of ", a);
+// while (i <= 10) {
+//   console.log(a * i);
+//   i++;
+// }
